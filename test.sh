1aaf967198677aa5fd0aaba7c6664a4f425fdfeb
ajitrajasekharan <ajitrajasekharan@icloud.com>
2022-06-16 18:11:44 +0000

    Use jq to see reasults in a readable fashion

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-python batch_main.py -input sentences.txt -output results.txt
+python3 batch_main.py -input sentences.txt -output results.txt
+apt-get install -y jq
+cat results.txt | jq > readable_results.txt
